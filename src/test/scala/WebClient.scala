e4fb367a57022324fe1cdb0d0cd3fc87d0c1b30e
wanglei <wanglei1@navercorp.com>
2019-01-24 15:18:56 +0800

    init Gatling Test client

---
+++ src/test/scala/WebClient.scala
@@ -0,0 +1,22 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._// 1
+
+class WebClient extends Simulation { // 3
+
+  val httpProtocol = http // 4
+    .baseUrl("http://localhost:8080") // 5
+    .acceptHeader("text/html,text/plain,application/json") // 6
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36")
+
+  val scn = scenario("BasicSimulation") // 7
+    .exec(http("webClient") // 8
+    .get("/webClient")) // 9
+    .pause(5) // 10
+
+  setUp( // 11
+    scn.inject(atOnceUsers(20)) // 12
+  ).protocols(httpProtocol) // 13
+}
